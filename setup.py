036605e01b761995cd545c5423597c315044e7ce
Andrea Bartezzaghi <andrea.bartezzaghi@gmail.com>
2022-09-06 00:19:26 +0200

    Added configs for formatting, linting and setup

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
+
 from setuptools import setup, find_packages
 
-setup(
-    name='ibwebapiclient',
-    version='0.1.0',
-    packages=find_packages(include=['ibwebapiclient']),
-    install_requires=[
-        "requests",
-        "websocket-client",
-        "coloredlogs",
-        "pydantic"
-    ]
-)
+if __name__ == "__main__":
+    setup(
+        name='ibwebapiclient',
+        version='0.1.0',
+        packages=find_packages(include=['ibwebapiclient']),
+        install_requires=[
+            "requests",
+            "websocket-client",
+            "coloredlogs",
+            "pydantic"
+        ]
+    )
